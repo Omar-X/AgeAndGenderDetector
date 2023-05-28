9de4216ee5889bcb6b92ca4401a0c20bd7c01818
Omar Beshir <omarbeshir25@gmail.com>
2023-05-28 20:42:19 +0300

    update

--- DetectorModuleGenerator.py
+++ DetectorModuleGenerator.py
@@ -1,43 +1,36 @@
-from keras.layers import Dense, Flatten, Conv2D, Dropout
-from keras.utils import to_categorical
-from keras.models import Sequential
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
+"""
+The Purpose of this program is to generate a model that can detect the age as well as the gender of a person
+from a given image. The model is trained on the UTKFace dataset. The model is saved as a .h5 file.
+"""
 import numpy as np
 import os
-from keras.layers import MaxPooling2D
 from tqdm import tqdm
 import random
-
-
-def age_group(age):
-    if 0 <= age < 10:
-        return 0
-    elif age < 20:
-        return 1
-    elif age < 30:
-        return 2
-    elif age < 40:
-        return 3
-    elif age < 50:
-        return 4
-    elif age < 60:
-        return 5
-    else:
-        return 6
-
-
-path = "/kaggle/input/utkface-new/UTKFace/"
-files = os.listdir(path)
-size = len(files)
-print("Total samples:", size)
-
 import cv2
 
-images = []
-ages = []
-genders = []
-for file in tqdm(files):
+from keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPooling2D  # Importing the required libraries
+from keras.utils import to_categorical  # for the model
+from keras.models import Sequential  # to be trained
+from sklearn.model_selection import train_test_split  # on the dataset
+import tensorflow as tf
+
+
+def age_group(age):  # Function to group the ages into 7 categories
+    if age >= 60:
+        return 6
+    else:
+        return int(age / 10)
+
+
+path = "UTKFace/"  # Path to the dataset
+filenames = os.listdir(path)
+size = len(filenames)
+print("Total samples:", size)
+
+images = []  # list to store the images
+ages = []  # list to store the ages
+genders = []  # list to store genders
+for file in tqdm(filenames):
     image = cv2.imread(path + file, 0)
     image = cv2.resize(image, dsize=(200, 200))
     image = image.reshape((image.shape[0], image.shape[1], 1))
@@ -46,48 +39,69 @@ for file in tqdm(files):
     ages.append(split_var[0])
     genders.append(int(split_var[1]))
 
-m = len(files)
-target = np.zeros((m, 7), dtype='float32')
-age = np.zeros((m, 1), dtype='float32')
-category_age = np.zeros((m, 7), dtype='float32')
+AgeTarget = np.zeros((size, 1), dtype='float32')
+GenderTarget = np.zeros((size, 1), dtype='float32')
+age = np.zeros((size, 1), dtype='float32')
+# category_age = np.zeros((size, 7), dtype='float32')
 
-features = np.zeros((m, 200, 200, 1), dtype='float32')
-randomnums = random.sample(range(0, m), m)
-for i in range(m):
+features = np.zeros((size, 200, 200, 1), dtype='float32')
+randomnums = random.sample(range(0, size), size)
+for i in range(size):
     age[i] = age_group(int(ages[randomnums[i]]))
+    GenderTarget[i] = genders[randomnums[i]]
     features[i] = images[randomnums[i]]
-category_age = to_categorical(age, 7)
-target[:, :] = category_age
-features = features / 255
+# add an extra dimension to the array to make it suitable for the model
+AgeTarget = age
+features = features / 255.0
 
-x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.2, shuffle=True)
 
+# split the dataset into training and testing sets
+x_train, x_test, y_age_train, y_age_test, y_gender_train, y_gender_test = train_test_split(features, AgeTarget,
+                                                                                           GenderTarget, test_size=0.2,
+                                                                                           random_state=42)
+
+# first and second convolutional layers
 model = Sequential()
 model.add(Conv2D(filters=32, kernel_size=7, padding="same", activation="relu", input_shape=(200, 200, 1)))
 model.add(Conv2D(filters=32, kernel_size=3, padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.25))
+
+# third and fourth convolutional layers
 model.add(Conv2D(filters=64, kernel_size=3, padding="same", activation="relu"))
 model.add(Conv2D(filters=128, kernel_size=3, padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=2))
+
+# fifth convolutional layer
 model.add(Conv2D(filters=128, kernel_size=3, padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.25))
+
+# flattening
 model.add(Flatten())
+
+# fully connected layers
 model.add(Dense(500, activation="relu"))
 model.add(Dropout(0.50))
 model.add(Dense(500, activation="relu"))
 model.add(Dropout(0.25))
 model.add(Dense(250, activation="relu"))
-model.add(Dense(7, activation="softmax"))
+
+# output layers
+model.add(Dense(1, activation="softmax", name="age_output"))
+model.add(Dense(1, activation="softmax", name="gender_output"))
 
 model.compile(loss=['binary_crossentropy'], optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-test_set = [x_test, y_test]
+
+# train the model
+test_set = [x_test, y_age_test, y_gender_test]
 csv_logger = tf.keras.callbacks.CSVLogger('agedetector.csv')
 
-model.fit(x_train, y_train, validation_data=test_set, epochs=5, batch_size=16, callbacks=[csv_logger])
+# train the model
+model.fit(x_train, [y_age_train, y_gender_train], validation_data=test_set, epochs=10, batch_size=32, callbacks=[csv_logger])
 
-dest = './agedetector.h5'
-model.save(dest)
+# save the model
+destFile = './AgeGenderDetector.h5'
+model.save(destFile)
